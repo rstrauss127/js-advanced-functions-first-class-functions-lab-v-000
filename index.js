36a91929856f96e641f01072ad482507d357eabf
Rebecca Strauss <rstrauss127@gmail.com>
2019-03-19 16:25:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const returnLastTwoDrivers= function(drivers) {
   return [drivers[drivers.length-2], drivers[drivers.length-1]];
 }
 
-const selectingDrivers = [returnFirstTwoDrivers(), returnLastTwoDrivers()];
-
+//const selectingDrivers = [returnFirstTwoDrivers(), returnLastTwoDrivers()];
+const selectingDrivers;
 function createFareMultiplier(int) {
   const multiplier = int;
   return function(fare) {
