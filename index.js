b92696fa24686cc6b4b6ff010f9c7aef3e979ee2
Rebecca Strauss <rstrauss127@gmail.com>
2019-03-19 16:12:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file!
+const x = function returnFirstTwoDrivers(drivers) {
+  const firstTwo = function(drivers) {
+    return [drivers[0], drivers[1]];
+  }
+  return firstTwo;
+}
+
+function returnLastTwoDrivers(drivers) {
+  const lastTwo = function(drivers) {
+    return [drivers[drivers.length], drivers[drivers.length-1]];
+  }
+  return lastTwo;
+}
+
+selectingDrivers = [returnFirstTwoDrivers(), returnLastTwoDrivers()];
+
+function createFareMultiplier(int) {
+  const multiplier = int;
+  return function(fare) {
+    return multiplier*fare;
+  }
+}
+
+function fareDoubler(fare) {
+  const doubler =  createFareMultiplier(2);
+  return doubler(fare);
+}
+
+function fareTripler(fare) {
+  const tripler = createFareMultiplier(3);
+  return tripler(fare);
+}
+
+function fetchSpecifiedDrivers(drivers, fn) {
+  return fn(drivers);
+}
